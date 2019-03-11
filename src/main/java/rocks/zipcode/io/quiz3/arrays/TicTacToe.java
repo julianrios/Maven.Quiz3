11d093d59ef0877282ce4c4e145105baae5cbe3e
Julian Rios <julianrios@J-No.localdomain>
2019-03-11 13:27:05 -0400

    passed a few tests in wave gen, pig latin gen, and tic tac toe

--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -1,22 +1,35 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class TicTacToe {
+    private String[][] board;
 
     public TicTacToe(String[][] board) {
+        this.board = board;
     }
 
     public TicTacToe() {
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        List<String> list = new ArrayList<>();
+        for (int rowNumber = 0; rowNumber <= value; rowNumber++) {
+            for (int columnNumber = 0; columnNumber < board.length; columnNumber++) {
+
+                list.add(board[rowNumber][columnNumber]);
+            }
+        }
+        String[] stringArray = list.toArray(new String[0]);
+        return stringArray;
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+        return getRow(value);
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
@@ -32,6 +45,6 @@ public class TicTacToe {
     }
 
     public String[][] getBoard() {
-        return null;
+        return this.board;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -1,10 +1,21 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import rocks.zipcode.io.quiz3.fundamentals.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class WaveGenerator {
     public static String[] wave(String str) {
-        return null;
+        str = str.toLowerCase();
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            list.add(StringUtils.capitalizeNthCharacter(str, i));
+        }
+        String[] stringArray = list.toArray(new String[0]);
+        return stringArray;
     }
 }
